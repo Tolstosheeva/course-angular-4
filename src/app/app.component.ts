26476d6acf9f05b555ab98cc3ab1e3f81a105f52
Tolstosheeva <tolstosheeva.m.v@gmail.com>
2020-12-09 14:27:38 +0200

    lesson3-4dz-new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,8 @@ import { Product } from './product.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+carts:Product [] = [];
+total:number = 0;
   view: string = 'grid';
 
   products: Product[] = [{
@@ -22,5 +23,21 @@ export class AppComponent {
 
   changeView(view: string) {
     this.view = view;
+  }
+  addToCart(product:Product){
+
+this.carts.push(product);
+this.totalCart();
+  }
+  totalCart(){
+    let total = 0;
+    this.carts.forEach((product)=>{
+      total+=product.price;
+    });
+    this.total = total;
+  }
+  delete(i: number){
+    this.carts.splice(i,1);
+    this.totalCart();
   }
 }
