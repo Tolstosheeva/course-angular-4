5209c38a0e4f626f0f03754986552eb41c1bf381
Tolstosheeva <tolstosheeva.m.v@gmail.com>
2020-12-14 23:49:22 +0200

    lessonDZ-todo-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule } from '@angular/common/http';
+import { TodoListComponent } from './todo-list/todo-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoListComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit {
+  items: string[] = [];
+  newTask!: string;
+  taskIsDone = false;
+
+
+
+  addToList() {
+    if (this.newTask == '') {
+    }
+    else {
+      this.items.push(this.newTask);
+      this.newTask = '';
+      
+    }
+  }
+
+  deleteTask(index: number) {
+    this.items.splice(index, 1);
+  }
+
+  successful() {
+    
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
