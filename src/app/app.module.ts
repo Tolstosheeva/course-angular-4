0a1aefbba0432bf8dad2c7e01881fbfdefb65038
Tolstosheeva <tolstosheeva.m.v@gmail.com>
2020-12-23 14:26:32 +0200

    error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRountingModule } from './app-routing.module';
+
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
@@ -12,7 +12,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    AppRountingModule,
+    
     BrowserAnimationsModule,
     HttpClientModule,
     AppModule
