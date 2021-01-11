f8a19b90881b996de42483f68e7b2bec70ecaa68
Tolstosheeva <tolstosheeva.m.v@gmail.com>
2021-01-11 16:15:42 +0200

    error

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Todo } from '../shared/models/product.models';
+import { TodoService } from '../shared/models/service/todo.service';
+
 
 @Component({
   selector: 'app-todo-list',
@@ -6,33 +10,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
-  items: string[] = [];
-  newTask!: string;
-  taskIsDone = false;
 
+  
+  todo: Todo[] = [];
 
+  todoForm!: FormGroup;
 
-  addToList() {
-    if (this.newTask == '') {
-    }
-    else {
-      this.items.push(this.newTask);
-      this.newTask = '';
-      
-    }
-  }
-
-  deleteTask(index: number) {
-    this.items.splice(index, 1);
-  }
-
-  successful() {
-    
-  }
-
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private todoService: TodoService
+  ) { }
 
   ngOnInit(): void {
+    this.todoForm = this.fb.group({
+      name: ''
+    });
+    this.todo = this.todoService.getTodo();
   }
+
+  addTodo() {
+    console.log(this.todoForm.value);
+    this.todoService.addTodoStorage(this.todoForm.value);
+    this.todoForm = this.fb.group({
+      name: ''
+    });
+  }
+
+  changeTodo(event: any) {
+    console.log(event.currentTarget.checked);
+    this.todoService.changeTodo(event);
+  }
+
+  deleteTodo(i: any) {
+    this.todoService.deleteTodo(i);
+  }
+
+  
 
 }
